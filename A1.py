cadec1f090bc6eaae97b1111df958f2ba9c8a75f
Sonia Nigam <sonianigam@sbcglobal.net>
2017-02-05 15:05:09 -0600

    new thresholds

--- A1.py
+++ A1.py
@@ -43,7 +43,7 @@ for camera in camera_images:
     canny_images = []
     for i in xrange(len(camera)):
         img = camera_images[camera][i]
-        edges = cv2.Canny(img,100,200)
+        edges = cv2.Canny(img,50,135)
         #print edges
         canny_images.append(edges)
     
@@ -57,31 +57,7 @@ for mask in masks:
     mask = ndi.gaussian_filter(mask, (10,10))
     mask = cv2.medianBlur(mask, 111)
 
-    # blur = cv2.GaussianBlur(mask,(5,5),0)
     ret4,th4 = cv2.threshold(mask,1,1,cv2.THRESH_BINARY)
-
-    #ret3,th3 = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    #image_final, contours, hierarchy = cv2.findContours(th4,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    #mask = cv2.drawContours(image_final, contours, -1, (0,255,0), 3)
-    
-    #####UNCOMMENT BELOW FOR AN ATTEMPT AT FOREGROUND IDENTIFICATION
-    # ret, thresh = cv2.threshold(mask,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    # # noise removal
-    # kernel = np.ones((3,3),np.uint8)
-    # opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-    #
-    # # sure background area
-    # sure_bg = cv2.dilate(opening,kernel,iterations=3)
-    #
-    # # Finding sure foreground area
-    # dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-    # ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-    #
-    # # Finding unknown region
-    # sure_fg = np.uint8(sure_fg)
-    # unknown = cv2.subtract(sure_bg,sure_fg)
-    # plt.imshow(sure_fg,cmap = 'gray')
-
 
     plt.imshow(th4,cmap = 'gray')
     plt.title('Camera Mask'), plt.xticks([]), plt.yticks([])
